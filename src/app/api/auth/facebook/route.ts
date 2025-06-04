0f22023d9da194ad8e372966ba69a3d69f166495
Na3ar-17 <gavruk2017@gmail.com>
2025-06-04 15:34:22 +0200

    fix: no code changes made

--- src/app/api/auth/facebook/route.ts
+++ src/app/api/auth/facebook/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server'
 export async function GET() {
 	const facebookAuthUrl = `https://www.facebook.com/v18.0/dialog/oauth?client_id=${process.env.FACEBOOK_APP_ID}&redirect_uri=${encodeURIComponent(
 		`${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/facebook/callback`
-	)}&scope=email,pages_read_engagement,pages_manage_posts,publish_to_groups,pages_show_list&response_type=code&state=random_state`
+	)}&scope=email,pages_read_engagement,pages_manage_posts,pages_show_list&response_type=code&state=random_state`
 
 	return NextResponse.redirect(facebookAuthUrl)
 }
